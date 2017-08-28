93ad20349920320fc6024f8a9bd9307e3e202ef4
A7xSV <sabhya.verma16@gmail.com>
2017-08-28 17:38:22 +0530

    Add BST-LevelOrder.cpp

---
+++ Codes/BST-LevelOrder.cpp
@@ -0,0 +1,24 @@
+#include <queue>
+
+void levelOrder(node * root)
+{
+    queue<node *> q;
+    q.push(root);
+    
+    while (!q.empty())
+    {
+        int n = q.size();
+        
+        while (n--)
+        {
+            node *temp = q.front();
+            q.pop();
+            cout << temp->data << " ";
+            
+            if (temp->left != NULL)
+                q.push(temp->left);
+            if (temp->right != NULL)
+                q.push(temp->right);
+        }
+    }
+}
