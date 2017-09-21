e83eeef2b9a8f1e11b9e9e4c4cc7918aac2ad8bb
A7xSV <sabhya.verma16@gmail.com>
2017-09-21 20:02:17 +0530

    Add Break.py

---
+++ Codes/Py Docs/Break.py
@@ -0,0 +1,9 @@
+import time
+import webbrowser
+
+for i in range (0,3):
+    print("Start time :"+time.ctime())
+    time.sleep(5)
+    webbrowser.open("https://www.youtube.com/watch?v=DelhLppPSxY")
+
+
